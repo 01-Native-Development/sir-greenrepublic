8931e6a6f331d46c07a24bab2b489f1588ae6ac7
Antonio-el-Bear <antoniozambuko@gmail.com>
2025-01-22 16:08:59 +0200

    template config

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
 
-// Load environment variables from .env file
-dotenv.config();
-
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  // Optional: You can add further configuration here if needed
+  integrations: [react()],
+  site: 'https://creativetimofficial.github.io',
+  base: '/astro-ecommerce'
 });
